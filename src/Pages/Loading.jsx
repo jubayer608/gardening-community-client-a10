01bae39f99a0a6f3690cfee89dbb72daf6935af1
jubayer608 <sheikhjubayer63@gmail.com>
2025-05-21 18:10:22 +0600

    added lottie loader

--- src/Pages/Loading.jsx
+++ src/Pages/Loading.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Lottie from "lottie-react";
 
-const Loading = () => {
+const Loader = () => {
+  const [animationData, setAnimationData] = useState(null);
+
+  useEffect(() => {
+    fetch("/loading.json") 
+      .then((res) => res.json())
+      .then((data) => setAnimationData(data));
+  }, []);
+
+  if (!animationData) return null;
+
   return (
-    <div className="min-h-screen flex  justify-center items-center">
-      <span className="loading loading-bars loading-xl"></span>
+    <div className="flex justify-center items-center min-h-[200px]">
+      <div className="w-32">
+        <Lottie animationData={animationData} loop={true} />
+      </div>
     </div>
   );
 };
 
-export default Loading;
+export default Loader;
