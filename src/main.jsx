acea3763630a7667fa0765f4b97cee768ca8d431
jubayer608 <sheikhjubayer63@gmail.com>
2025-05-21 18:05:04 +0600

    added lottie npm package

--- src/main.jsx
+++ src/main.jsx
@@ -9,12 +9,10 @@ import AuthProvider from './provider/AuthProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-   
-    <AuthProvider>
-      
+
+   <AuthProvider> 
     <RouterProvider router={router}></RouterProvider>
-    
    </AuthProvider>
-  
+   
   </StrictMode>,
 )
